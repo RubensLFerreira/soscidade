14b59724ee1d3e448415bf57e6c6f4c34afa8729
RubensLFerreira <rubenslima000@gmail.com>
2023-07-04 01:23:08 -0300

    aplicando globalStyle

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from "./App";
+import App from './App';
 
-import "./index.css";
+import GlobalStyle from './styles/global';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
