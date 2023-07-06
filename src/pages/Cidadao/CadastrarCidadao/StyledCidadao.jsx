a72c9199c489237c091a636011f03157c49c5c01
RubensLFerreira <rubenslima000@gmail.com>
2023-07-06 17:53:14 -0300

    refactor: corrigindo sintaxe

--- src/pages/Cidadao/CadastrarCidadao/StyledCidadao.jsx
+++ src/pages/Cidadao/CadastrarCidadao/StyledCidadao.jsx
@@ -1,16 +1,16 @@
 import { Box, TextField, Button, styled, Typography } from '@mui/material';
 
-export const StyledBox  = styled(Box) ({
+export const StyledBox = styled(Box)({
   backgroundColor: '#ffffff',
   width: '400px',
   margin: '3rem auto 0 auto',
   textAlign: 'center',
   padding: '3rem 1rem',
-  borderRadius: '10px'
+  borderRadius: '10px',
 });
 
 export const StyledTextField1 = styled(TextField)({
-  width: "80%",
+  width: '80%',
 });
 
 export const StyledTypography = styled(Typography)({
@@ -24,15 +24,15 @@ export const StyledTypography2 = styled(Typography)({
   color: '#2e2e2e',
   fontFamily: 'Arial, Helvetica, sans-serif',
   textTransform: 'uppercase',
-  fontSize: '10pt'
+  fontSize: '10pt',
 });
 
-export const StyledButton = styled(Button) ({
+export const StyledButton = styled(Button)({
   width: '80%',
   height: '50px',
   color: '#f5f5f5',
   backgroundColor: '#3b8efc',
   '&:hover': {
-    backgroundColor: '#1e74e6'
-  }
+    backgroundColor: '#1e74e6',
+  },
 });
