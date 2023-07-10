6e3c7d10b606965870d166db60b13664b753e8ed
RubensLFerreira <rubenslima000@gmail.com>
2023-07-09 23:42:51 -0300

    feat: serviços de problemas pendentes e finalziados

--- src/service/ProblemasService.js
+++ src/service/ProblemasService.js
@@ -5,6 +5,27 @@ export const todosProblemas = async () => {
 		const problemas = await api.get('/problemas');
 		return problemas.data;
 	} catch (error) {
-		console.log('[Erro] Service/problemas', error);
+		console.log('Erro no ProblemasServices!');
+		return { message: error };
+	}
+};
+
+export const problemasPendentes = async () => {
+	try {
+		const pendentes = await api.get('/problemas/pendentes');
+		return pendentes.data;
+	} catch (error) {
+		console.log('Erro no ProblemasServices!');
+		return { message: error };
+	}
+};
+
+export const problemasFinalizados = async () => {
+	try {
+		const finalizados = await api.get('/problemas/finalizados');
+		return finalizados.data;
+	} catch (error) {
+		console.log('Erro no ProblemasServices!');
+		return { message: error };
 	}
 };
