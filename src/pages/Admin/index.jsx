8ccea384d2d2840af319b1c7566cca954d4af2b6
RubensLFerreira <rubenslima000@gmail.com>
2023-07-10 23:51:34 -0300

    refactor: corrigindo components

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,42 +1,46 @@
-import ListaDenuncias from '../../components/Admin/ListaDenuncias';
+import ListaProblemas from '../../components/Admin/ListaProblemas';
 import ListaUsuarios from '../../components/Admin/ListaUsuarios';
 import InputSearch from '../../components/Admin/InputSearch';
-// import TabelaCidadaos from '../../components/Admin/TabelaCidadaos';
+import TabelaCidadaos from '../../components/Admin/TabelaCidadaos';
+import TabelaPrefeituras from '../../components/Admin/TabelaPrefeituras';
 import TabelaProblemas from '../../components/Admin/TabelaProblemas';
-import DenunciasPendentes from '../../components/Admin/DenunciasPendentes';
+import TabelaProblemasFinalizados from '../../components/Admin/TabelaProblemasFinalizados';
+import TabelaProblemasPendentes from '../../components/Admin/TabelaProblemasPendentes';
 
 import {
-  StyledBox,
-  StyledGrid,
-  StyledGrid1,
-  StyledGrid2,
-  StyledGrid3,
+	StyledBox,
+	StyledGrid,
+	StyledGrid1,
+	StyledGrid2,
+	StyledGrid3,
 } from './StyledAdmin';
 import { Typography } from '@mui/material';
 
 export default function Admin() {
-  return (
-    <StyledBox>
-      <StyledGrid container>
-        <StyledGrid1>
-          <Typography
-            style={{ textAlign: 'center', margin: '2rem 0' }}
-            variant="h4"
-          >
-            Dashboard
-          </Typography>
-          <InputSearch />
-          <ListaUsuarios />
-          <ListaDenuncias />
-        </StyledGrid1>
-        <StyledGrid2>
-          {/* <TabelaCidadaos /> */}
-					<TabelaProblemas />
-        </StyledGrid2>
-        <StyledGrid3>
-          {/* <DenunciasPendentes /> */}
-        </StyledGrid3>
-      </StyledGrid>
-    </StyledBox>
-  );
+	return (
+		<StyledBox>
+			<StyledGrid container>
+				<StyledGrid1>
+					<Typography
+						style={{ textAlign: 'center', margin: '2rem 0' }}
+						variant="h4"
+					>
+						Dashboard
+					</Typography>
+					<InputSearch />
+					<ListaUsuarios />
+					<ListaProblemas />
+				</StyledGrid1>
+				<StyledGrid2>
+					{/* <TabelaCidadaos /> */}
+					{/* <TabelaPrefeituras /> */}
+					{/* <TabelaProblemas /> */}
+				</StyledGrid2>
+				<StyledGrid3>
+					{/* <TabelaProblemasFinalizados /> */}
+					{/* <TabelaProblemasPendentes /> */}
+				</StyledGrid3>
+			</StyledGrid>
+		</StyledBox>
+	);
 }
