89b9fee09f0fb43e485b587dc65b16aa7444a9be
RubensLFerreira <rubenslima000@gmail.com>
2023-07-10 23:48:07 -0300

    feat: add lista de problemas

---
+++ src/components/Admin/ListaProblemas/index.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import {
+	List,
+	ListItem,
+	ListItemText,
+	Divider,
+	Typography,
+} from '@mui/material';
+
+const style = {
+	width: '100%',
+	maxWidth: 360,
+	bgcolor: 'background.paper',
+};
+
+export default function ListaProblemas() {
+	return (
+		<div style={{ margin: '1rem 0' }}>
+			<Typography variant="h5" style={{ textAlign: 'left' }}>
+				Lista de Denúncias
+			</Typography>
+			<List sx={style} component="nav" aria-label="mailbox folders">
+				<ListItem button>
+					<ListItemText primary="Finalizadas" />
+				</ListItem>
+				<Divider light />
+				<ListItem button>
+					<ListItemText primary="Pendentes" />
+				</ListItem>
+			</List>
+		</div>
+	);
+}
