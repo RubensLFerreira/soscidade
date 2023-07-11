85b54b54a02c22b134e78de2c8123c9c17a8a024
RubensLFerreira <rubenslima000@gmail.com>
2023-07-11 18:27:01 -0300

    feat: troca dinâmica de tabelas

--- src/pages/Admin/StyledAdmin.jsx
+++ src/pages/Admin/StyledAdmin.jsx
@@ -1,52 +1,71 @@
-import { styled, Box, Grid, TextField } from '@mui/material';
+import { styled, Box, Grid, TextField, List, Typography } from '@mui/material';
 
 export const StyledBox = styled(Box)({
-  backgroundColor: '#ffffff',
-  width: '80%',
-  margin: '3rem auto 0 auto',
-  textAlign: 'center',
-  padding: '3rem 1rem',
-  borderRadius: '10px',
+	backgroundColor: '#ffffff',
+	width: '80%',
+	margin: '3rem auto 0 auto',
+	textAlign: 'center',
+	padding: '3rem 1rem',
+	borderRadius: '10px',
 });
 
 export const StyledGrid = styled(Grid)({
-  display: 'grid',
-  gridTemplateColumns: '1fr 3fr',
-  gridTemplateRows: 'repeat(2, 1fr)',
-  gridColumnGap: '8px',
-  gridRowGap: '1rem',
+	display: 'grid',
+	gridTemplateColumns: '1fr 3fr',
+	gridTemplateRows: 'repeat(2, 1fr)',
+	gridColumnGap: '8px',
+	gridRowGap: '1rem',
 });
 
 export const StyledGrid1 = styled(Grid)({
-  gridArea: '1 / 1 / 3 / 2',
-  // backgroundColor: '#fc9a9a',
-  borderRadius: '10px 0 0 10px',
+	gridArea: '1 / 1 / 3 / 2',
+	// backgroundColor: '#fc9a9a',
+	borderRadius: '10px 0 0 10px',
 });
 
 export const StyledGrid2 = styled(Grid)({
-  gridArea: '1 / 2 / 2 / 3',
-  // backgroundColor: '#9ad8fc',
-  height: '100%',
-  borderRadius: '0 10px 0 0',
+	gridArea: '1 / 2 / 2 / 3',
+	// backgroundColor: '#9ad8fc',
+	height: '100%',
+	borderRadius: '0 10px 0 0',
 });
 
 export const StyledGrid3 = styled(Grid)({
-  gridArea: '2 / 2 / 3 / 3',
-  // backgroundColor: '#9ad8fc',
-  borderRadius: '0 0 10px 0',
-  height: '100%',
+	gridArea: '2 / 2 / 3 / 3',
+	// backgroundColor: '#9ad8fc',
+	borderRadius: '0 0 10px 0',
+	height: '100%',
+});
+
+export const StyledTypography1 = styled(Typography)({
+	textAlign: 'center',
+	margin: '2rem 0',
+	fontSize: '20pt',
+});
+
+export const StyledTypography2 = styled(Typography)({
+	textAlign: 'left',
+	fontSize: '15pt',
 });
 
 // Barra de pesquisa
 
 export const InputSearch = styled('input')({
-  width: '80%',
-  height: '3rem',
-  marginTop: '2rem',
-  borderRadius: '10px',
-  fontSize: '13pt',
-  padding: '0 1rem',
-  border: 'none',
-  boxShadow: '0 0 0 0',
-  outline: '0',
+	width: '80%',
+	height: '3rem',
+	marginTop: '2rem',
+	borderRadius: '10px',
+	fontSize: '13pt',
+	padding: '0 1rem',
+	border: 'none',
+	boxShadow: '0 0 0 0',
+	outline: '0',
+});
+
+// Listas
+
+export const ListStyle = styled(List)({
+	width: '100%',
+	maxWidth: 360,
+	bgcolor: 'background.paper',
 });
--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,5 +1,3 @@
-import ListaProblemas from '../../components/Admin/ListaProblemas';
-import ListaUsuarios from '../../components/Admin/ListaUsuarios';
 import InputSearch from '../../components/Admin/InputSearch';
 import TabelaCidadaos from '../../components/Admin/TabelaCidadaos';
 import TabelaPrefeituras from '../../components/Admin/TabelaPrefeituras';
@@ -13,32 +11,87 @@ import {
 	StyledGrid1,
 	StyledGrid2,
 	StyledGrid3,
+	StyledTypography1,
+	StyledTypography2,
+	ListStyle,
 } from './StyledAdmin';
-import { Typography } from '@mui/material';
+
+import { useState } from 'react';
+
+import { ListItem, ListItemText, Divider, Typography } from '@mui/material';
 
 export default function Admin() {
+	const [buttonAtive1, setButtonAtive1] = useState('button1');
+	const [buttonAtive2, setButtonAtive2] = useState('button4');
+
+	const buttonClick1 = (buttonId1) => {
+		setButtonAtive1(buttonId1);
+	};
+
+	const buttonClick2 = (buttonId2) => {
+		setButtonAtive2(buttonId2);
+	};
+
 	return (
 		<StyledBox>
 			<StyledGrid container>
 				<StyledGrid1>
-					<Typography
-						style={{ textAlign: 'center', margin: '2rem 0' }}
-						variant="h4"
-					>
-						Dashboard
-					</Typography>
+					<StyledTypography1>Dashboard</StyledTypography1>
 					<InputSearch />
-					<ListaUsuarios />
-					<ListaProblemas />
+
+					<div style={{ margin: '2rem 0' }}>
+						<StyledTypography2>Lista de usuários</StyledTypography2>
+						<ListStyle component="nav" aria-label="mailbox folders">
+							<ListItem button>
+								<ListItemText
+									primary="Cidadãos"
+									onClick={() => buttonClick1('button1')}
+								/>
+							</ListItem>
+							<Divider />
+							<ListItem button divider>
+								<ListItemText
+									primary="Prefeituras"
+									onClick={() => buttonClick1('button2')}
+								/>
+							</ListItem>
+							<Divider light />
+							<ListItem button>
+								<ListItemText
+									primary="Denúncias"
+									onClick={() => buttonClick1('button3')}
+								/>
+							</ListItem>
+						</ListStyle>
+					</div>
+
+					<div style={{ margin: '1rem 0' }}>
+						<StyledTypography2>Lista de Denúncias</StyledTypography2>
+						<ListStyle component="nav" aria-label="mailbox folders">
+							<ListItem button>
+								<ListItemText
+									primary="Finalizadas"
+									onClick={() => buttonClick2('button4')}
+								/>
+							</ListItem>
+							<Divider light />
+							<ListItem button>
+								<ListItemText
+									primary="Pendentes"
+									onClick={() => buttonClick2('button5')}
+								/>
+							</ListItem>
+						</ListStyle>
+					</div>
 				</StyledGrid1>
 				<StyledGrid2>
-					{/* <TabelaCidadaos /> */}
-					{/* <TabelaPrefeituras /> */}
-					{/* <TabelaProblemas /> */}
+					{buttonAtive1 === 'button1' && <TabelaCidadaos />}
+					{buttonAtive1 === 'button2' && <TabelaPrefeituras />}
+					{buttonAtive1 === 'button3' && <TabelaProblemas />}
 				</StyledGrid2>
 				<StyledGrid3>
-					{/* <TabelaProblemasFinalizados /> */}
-					{/* <TabelaProblemasPendentes /> */}
+					{buttonAtive2 === 'button4' && <TabelaProblemasFinalizados />}
+					{buttonAtive2 === 'button5' && <TabelaProblemasPendentes />}
 				</StyledGrid3>
 			</StyledGrid>
 		</StyledBox>
