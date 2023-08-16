878b0c44ff409d6c60f043da27e6fd23d7ad1572
RubensLFerreira <rubenslima000@gmail.com>
2023-08-16 18:09:09 -0300

    feat: tela de informações

---
+++ src/pages/Informacao/StyledInformacao.jsx
@@ -0,0 +1,58 @@
+import {
+	Box,
+	styled,
+	Typography,
+	TextField,
+	Button,
+	Autocomplete,
+} from '@mui/material';
+
+export const StyledImgContainer = styled('div')({
+	width: '100%',
+	height: '200px',
+	overflow: 'hidden',
+	backgroundImage:
+		'url(https://www.pit.ac.in/assets/images/ContactUs/contact-us-banner.jpg)',
+	backgroundSize: 'cover',
+	backgroundPosition: 'center center',
+	boxShadow: '2px 2px 8px black'
+});
+
+export const StyledImg = styled('img')({
+	backgroundColor: '#214291',
+	backgroundRepeat: 'no-repeat',
+	width: '100%',
+	backgroundPosition: 'left 76px',
+	filter: 'brightness(40%)',
+});
+
+export const StyledTypography1 = styled(Typography)({
+	color: '#322153',
+	fontSize: '25pt',
+	fontWeight: 'bold',
+	textAlign: 'center',
+});
+
+
+export const StyledBox = styled(Box)({
+	backgroundColor: 'white',
+	maxWidth: '900px',
+	width: '70vw',
+	margin: '3rem auto 5rem auto',
+	padding: '1rem 2rem',
+	borderRadius: '10px',
+	textAlign: 'justify'
+});
+
+export const StyledBox2 = styled(Box)({
+	margin: '2rem 0',
+	display: 'flex',
+	flexWrap: 'wrap',
+	'& > :not(style)': {
+		m: 1,
+		width: 228,
+		height: 228,
+		margin: 10,
+	},
+	justifyContent: 'space-around',
+});
---
+++ src/pages/Informacao/index.jsx
@@ -0,0 +1,61 @@
+import {
+	StyledBox,
+	StyledTypography1,
+	StyledImgContainer,
+} from './StyledInformacao';
+
+import { Grid } from '@mui/material';
+
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+
+export default function Informacao() {
+	return (
+		<>
+			<Navbar />
+
+			<StyledImgContainer></StyledImgContainer>
+			<StyledBox>
+				<Grid container spacing={2}>
+					<Grid item xs={12}>
+						<StyledTypography1>Entre em contato conosco</StyledTypography1>
+					</Grid>
+
+					<Grid item xs={12}>
+						O SOSCidade surge como uma proposta de sistema que visa solucionar
+						os problemas de infraestrutura urbana, que se referem às estruturas
+						e serviços necessários para o funcionamento de uma cidade,
+						considerando aspectos ambientais, sociais e econômicos. A
+						importância do SOSCidade está em fornecer uma solução eficiente para
+						as questões encontradas nos municípios, permitindo que os cidadãos
+						reportem problemas em suas comunidades e que as autoridades
+						governamentais possam responder de maneira ágil e eficaz.
+					</Grid>
+
+					<Grid item xs={12}>
+						O sistema supracitado aborda desafios importantes da nossa realidade
+						urbana, como avenidas e ruas esburacadas, falta de calçadas,
+						iluminação pública precária, saneamento básico deficiente, ciclovias
+						ausentes, coleta de lixo ineficiente, problemas de trânsito e pontes
+						e viadutos em mal estado. A plataforma promoverá a participação
+						ativa dos cidadãos na construção de cidades mais sustentáveis, com
+						foco na melhoria da qualidade de vida.
+					</Grid>
+
+					<Grid item xs={12}>
+						Com o uso do SOSCidade, será possível monitorar e avaliar áreas
+						problemáticas por meio da geolocalização, permitindo a tomada de
+						decisões mais assertivas e a execução de intervenções mais ágeis e
+						precisas. Isso resulta na construção de cidades mais sustentáveis e
+						seguras para todos. Além disso, a utilização de tecnologias como
+						esse projeto permitirá uma maior participação e engajamento da
+						sociedade no processo de planejamento e gestão urbana, contribuindo
+						para uma maior transparência e efetividade das políticas públicas.
+					</Grid>
+				</Grid>
+			</StyledBox>
+
+			<Footer />
+		</>
+	);
+}
